4f52de8d722c00ae9edd655e47e52cd024d0bcde
Juan Pablo Lassala <jpablolassala@gmail.com>
2024-03-06 20:59:19 -0300

    Add morgan

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express, { Express } from "express";
 import { InitializeMiddlewares } from "./App";
+import morgan from "morgan";
 
 const app: Express = express();
 const port = 3000;
 
 InitializeMiddlewares(app);
+
+app.use(morgan("combined"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
