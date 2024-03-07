d4f6bfb4c9b724dfebfd7d4fe518ee8f97d905e8
Juan Pablo Lassala <jpablolassala@gmail.com>
2024-03-07 15:42:23 -0300

    Move morgan initialization

--- src/App.ts
+++ src/App.ts
@@ -4,9 +4,11 @@ import { createProductoRoutes } from "./Productos/ProductoRoutes";
 import { createInstances } from "./Container";
 import { createMarcaRoutes } from "./Marcas/MarcaRoutes";
 import { createImageRoutes } from "./Images/ImageRoutes";
+import morgan from "morgan";
 
 export function InitializeMiddlewares(app: Express): void {
   app.use(cors());
+  app.use(morgan("combined"));
 
   const { marcaHandler, productoHandler, imageHandler } = createInstances();
   app.use("/", createProductoRoutes(productoHandler));
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 import express, { Express } from "express";
 import { InitializeMiddlewares } from "./App";
-import morgan from "morgan";
 import { envConfig } from "./config/env";
 
 const app: Express = express();
 
 InitializeMiddlewares(app);
 
-app.use(morgan("combined"));
 app.listen(envConfig.port, () => console.log(`Example app listening on port ${envConfig.port}`));
