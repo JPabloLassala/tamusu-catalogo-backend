cd691de0f524a3b1244eb80a99b1f831290f2a50
Juan Pablo Lassala <jpablolassala@gmail.com>
2024-03-07 15:41:53 -0300

    Add dotenv

--- src/config/env.ts
+++ src/config/env.ts
@@ -6,11 +6,13 @@ export interface Config {
   db: {
     path: string;
   };
+  pageSize: number;
 }
 
 export const envConfig: Config = {
   port: parseInt(process.env.PORT!),
   env: process.env.NODE_ENV!,
+  pageSize: parseInt(process.env.PAGE_SIZE!),
   db: {
     path: process.env.DB_PATH!,
   },
