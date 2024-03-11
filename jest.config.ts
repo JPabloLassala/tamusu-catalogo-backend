dece65b5eb23008d46ca465c00d48dd607cbf6e5
Juan Pablo Lassala <jpablolassala@gmail.com>
2024-03-11 15:13:47 -0300

    Add ts-jest preset

--- jest.config.ts
+++ jest.config.ts
@@ -6,14 +6,8 @@
 import type { Config } from "jest";
 
 const config: Config = {
-  collectCoverage: true,
-  coverageDirectory: "coverage",
-  coverageProvider: "v8",
-  // The glob patterns Jest uses to detect test files
-  testMatch: ["/**/?(*.)+(spec|test).ts"],
-  // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  testPathIgnorePatterns: ["/node_modules/"],
-  collectCoverageFrom: ["src/**/*Handler.ts"],
+  preset: "ts-jest",
+  testEnvironment: "node",
 };
 
 export default config;
