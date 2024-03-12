a9c8a7d88ac7e33b257ae178d23eb2a8af75a888
Juan Pablo Lassala <jpablolassala@gmail.com>
2024-03-12 16:54:59 -0300

    Add swagger

--- src/App.ts
+++ src/App.ts
@@ -6,10 +6,13 @@ import { createMarcaRoutes } from "./Marcas/MarcaRoutes";
 import { createImageRoutes } from "./Images/ImageRoutes";
 import { globalExceptionLayer } from "./Error";
 import { morganMiddleware } from "./config/morgan";
+import { JsonObject, serve, setup } from "swagger-ui-express";
+import swaggerDocument from "./swagger.json";
 
 export function InitializeMiddlewares(app: Express): void {
   app.use(cors());
   app.use(morganMiddleware);
+  app.use("/api-docs", serve, setup(swaggerDocument as JsonObject));
 
   const { marcaHandler, productoHandler, imageHandler } = createInstances();
   app.use("/", createProductoRoutes(productoHandler));
